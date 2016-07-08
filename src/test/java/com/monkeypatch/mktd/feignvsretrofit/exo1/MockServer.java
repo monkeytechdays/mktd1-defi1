5461762583adcd3fa7f8ae2d4bbfaa394becfd0f
Vincent LALANNE <vlalanne90@gmail.com>
2016-07-08 11:47:31 +0200

    exo7

--- src/test/java/com/monkeypatch/mktd/feignvsretrofit/exo1/MockServer.java
+++
@@ -1,7 +0,0 @@
-package com.monkeypatch.mktd.feignvsretrofit.exo1;
-
-/**
- * Created by vincent on 08/07/16.
- */
-public class MockServer {
-}
--- src/test/java/com/monkeypatch/mktd/feignvsretrofit/exo1/_07_Bonus.java
+++ src/test/java/com/monkeypatch/mktd/feignvsretrofit/exo1/_07_Bonus.java
@@ -1,6 +1,8 @@
 package com.monkeypatch.mktd.feignvsretrofit.exo1;
 
 import com.monkeypatch.mktd.feignvsretrofit.exo1.model.Monkey;
+import com.monkeypatch.mktd.feignvsretrofit.exo1.model.MonkeyRace;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -9,21 +11,33 @@ import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
 import java.util.stream.Collectors;
 
 import static com.monkeypatch.mktd.feignvsretrofit.exo1.ApiFactory.buildMonkeyApi;
 import static com.monkeypatch.mktd.feignvsretrofit.exo1.ApiFactory.buildRaceApi;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
-public class _07_Bonus {
+public class _07_Bonus implements TestsConfiguration {
 
     // TODO you should add a test rule here
     private MonkeyRaceApi raceApi;
     private MonkeyApi monkeyApi;
+    MockMonkey mock = new MockMonkey();
+
+    @After
+    public void after() {
+        mock.stopMock();
+    }
 
     @Before
     // TODO you should update this configuration
     public void before() throws Exception {
-        String url = null;
+        String url = "http://localhost:4567";
+
+        mock.startMock();
+
         raceApi = buildRaceApi(url);
         monkeyApi = buildMonkeyApi(url);
     }
@@ -31,7 +45,10 @@ public class _07_Bonus {
     @Test
     // TODO you should implements this test
     public void testGetRaces() {
-        raceApi.getMonkeyRaces();
+        List<MonkeyRace> races = raceApi.getMonkeyRaces();
+
+        assertNotNull(races);
+        assertEquals(RACES_NUMBER, races.size());
     }
 
     @Test
